03d029a5a454feef7d33337bd06a3a3edcbd9ef5
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-07 14:50:32 +1200

    Meta changes

--- good-sign-offs/src/pages/index.tsx
+++ good-sign-offs/src/pages/index.tsx
@@ -7,10 +7,25 @@ function Home() {
       <Head>
         <title>Good Sign-Offs</title>
         <meta property="og:title" content="Good Sign-Offs" key="title" />
+        <meta property="twitter:title" content="Good Sign-Offs"/>
+        <meta
+          property="description"
+          content="A mini experiment powered by are.na"
+        />
+        <meta
+          property="og:description"
+          content="A mini experiment powered by are.na"
+        />
+        <meta
+          property="twitter:description"
+          content="A mini experiment powered by are.na"
+        />
+        <meta property="og:url" content="https://good-sign-offs.tom.so" />
+        <meta property="og:image" content="og.png"/>
       </Head>
-      <RandomBlock/>
+      <RandomBlock />
     </div>
-  )
+  );
 };
 
 export default Home;
