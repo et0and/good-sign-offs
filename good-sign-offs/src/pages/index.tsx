68b918676e154c3e9b78308e964e9a63494ae85a
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-16 23:52:04 +1200

    Path

--- good-sign-offs/src/pages/index.tsx
+++ good-sign-offs/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import RandomBlock from "@/components/RandomBlock";
-import Head from 'next/head'
+import Head from "next/head";
 
 function Home() {
   return (
@@ -7,7 +7,7 @@ function Home() {
       <Head>
         <title>Good Sign-Offs</title>
         <meta property="og:title" content="Good Sign-Offs" key="title" />
-        <meta property="twitter:title" content="Good Sign-Offs"/>
+        <meta property="twitter:title" content="Good Sign-Offs" />
         <meta name="theme-color" content="#FFEDD5" />
         <meta
           property="description"
@@ -22,16 +22,15 @@ function Home() {
           content="A mini experiment powered by are.na"
         />
         <meta property="og:url" content="https://work.tom.so/good-sign-offs" />
-        <meta property="og:image" content="og.png" />
+        <meta property="og:image" content="/good-sign-offs/og.png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="og.png"/>
+        <meta property="twitter:image" content="/good-sign-offs/og.png" />
       </Head>
       <div className="bg-orange-100">
-      <RandomBlock />
+        <RandomBlock />
       </div>
     </div>
   );
-};
+}
 
 export default Home;
-
